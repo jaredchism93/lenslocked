7fadcb73ca10712f5a6ee3bc3924d05a547a0b1c
Jared Chism <jaredchism93@gmail.com>
2021-02-04 19:17:36 -0500

    main.go now a webserver

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
+func handlerFunc(w  http.ResponseWriter, r *http.Request){
+	fmt.Fprint(w, "<h1>yo yo yo</h1>")
+}
+
 func main() {
-	fmt.Println("Hello Jared")
+	http.HandleFunc("/", handlerFunc)
+	http.ListenAndServe(":3000", nil)
+
 }
